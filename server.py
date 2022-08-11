b44d5945be94f8d9ec3d2e09bcf4a8329a46369c
JesseIyukeWits <88620378+JesseIyukeWits@users.noreply.github.com>
2022-08-11 23:56:03 +0200

    updated server 1

--- server.py
+++ server.py
@@ -2,14 +2,7 @@ import socket
 import threading
 import time
 import datetime
-
-import binascii
 import traceback
-
-
-#from decoder import decoder #import functions decoder file in same folder directory
-
-#avlDecoder= decoder()
 
 
 #constants
@@ -68,6 +61,8 @@ class TCP(threading.Thread):
                 imeiData = self.commandSocket.recv(SIZE)
                 if(imeiData):
                     imei = imeiData.decode(FORMAT)
+                    imei=str(imei)
+                    imei= imei.split("0F")[1]
                     print(imei+ " :imei data") #E.g - 000F333536333037303432343431303133
                     self.Communicator(self.commandSocket, imei)
                 else:
@@ -79,21 +74,21 @@ class TCP(threading.Thread):
 
     def Communicator(self, conn, imei):
         accept_con_mes = '\x01' #acceptance code
-        conn.send(accept_con_mes.encode(FORMAT))
+        conn.send(bytes(accept_con_mes,FORMAT)) 
         print("Device connected")
         while True:
             try:
                 data = conn.recv(SIZE)
+                print(data)
                 if(data):
                     vars = {} # store in dictionary
-                    vars['imei'] = imei.split("\x0f")[1]
-                    recieved = self.decoder(data) # convert to hexadecimal form
-                    with open('raw.txt', 'a+') as w: # testing purposes
-                        w.writelines(recieved.decode(FORMAT)+'\n')
-                    vars.update(self.decodeAVL(recieved))  #AVL decoding   
+                    imeiNo = {'IMEI':imei}
+                    vars.update(imeiNo)
+                    vars.update(self.decodeAVL(data))  #AVL decoding 
+                    print(vars)
+                    
                     resp = self.mResponse(vars['NumberofData1']) #check for number of records sent is correct
-                    time.sleep(30)
-                    conn.send(resp)
+                    conn.sendall(resp)
                 else:
                     break
             except Exception as e:
@@ -103,29 +98,27 @@ class TCP(threading.Thread):
         print('exiting tcp comms')
 
     
-    def decoder(self, raw):
-        decoded = binascii.hexlify(raw)
-        return decoded
-    
     def decodeAVL(self, data):
         self.preamble = int(data[0:7]) #the packet starts with four zero bytes.
-        self.data_field_l  = int(data[8:15]) #?  Data Field Length – size is calculated starting from Codec ID to Number of Data 2.
-        self.codecid = int(data[16:17])  # codecid
-        self.NumberofData1 = int(data[18:19]) # Number of Data 1 – a number which defines how many records is in the packet
-        self.NumberofData2 = int(data[-10:-9])  #? no of total records before crc-16 check
-        self.crc_16 = int(data[-8:])  # crc-16 check- the last 4 bytes
+        self.data_field_l  = int(data[8:16],16) #?  Data Field Length – size is calculated starting from Codec ID to Number of Data 2.
+        self.codecid = int(data[16:18],16)  # codecid
+        self.NumberofData1 = int(data[18:20],16) # Number of Data 1 – a number which defines how many records is in the packet
+        self.NumberofData2 = int(data[-10:-8],16)  #? no of total records before crc-16 check
+        self.crc_16 = int(data[-8:],16)  # crc-16 check- the last 4 bytes
+
+        print(self.codecid)
     
         if(self.codecid == 8 and (self.NumberofData1 == self.NumberofData2)):
 
-            self.d_time_unix  = int(data[20:27])  # device time unix
-            self.d_time_local = self.unixtoLocal(self.d_time_unix)  # device time local
-            self.priority     = int(data[28:29])    # device data priority
-            self.lon          = int(data[30:37])  # longitude
-            self.lat          = int(data[38:45]) # latitude
-            self.alt          = int(data[46:50]) # altitude
-            self.angle        = int(data[51:54]) # angle
-            self.satellites   = int(data[55:56])  # no of satellites
-            self.speed        = int(data[57:60]) # speed
+            self.d_time_unix  = int(data[20:36],16)  # device time unix
+            self.d_time_local = self.unixtoLocal(self.d_time_unix)  # device time local- SA time
+            self.priority     = int(data[36:38],16)    # device data priority
+            self.lon          = int(data[38:46],16)  # longitude
+            self.lat          = int(data[46:54],16) # latitude
+            self.alt          = int(data[54:58],16) # altitude
+            self.angle        = int(data[58:62],16) # angle
+            self.satellites   = int(data[62:64],16)  # no of satellites
+            self.speed        = int(data[64:68],16) # speed
 
             return self.getAvlData()
         else:
@@ -139,8 +132,8 @@ class TCP(threading.Thread):
     def getAvlData(self):
         data = {
             "codecid"    : self.codecid,
-            "no_record_i": self.NumberofData1,
-            "no_record_e": self.NumberofData2,
+            "NumberofData1": self.NumberofData1,
+            "NumberofData2": self.NumberofData2,
             "crc-16"     : self.crc_16,
             "d_time_unix" : self.d_time_unix,
             "d_time_local": self.d_time_local,
@@ -157,8 +150,6 @@ class TCP(threading.Thread):
     def mResponse(self, data):
         return data.to_bytes(4, byteorder = 'big')
 
-
-
 #Start threading
 def startServer( ):
     listenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #
@@ -167,6 +158,7 @@ def startServer( ):
     listenSock.bind((ADDRESS))
     listenSock.listen()
     print('Server started', 'Listen on: %s, %s' % listenSock.getsockname( ))
+    i = 0
     while True:
         connection, address = listenSock.accept( )
         i +=1
