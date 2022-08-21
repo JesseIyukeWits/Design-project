8c0965b26ab5d8c498802ba87854bb7516a993e7
Ishvani Moodley <ishvani.moodley@students.wits.ac.za>
2022-08-21 19:51:18 +0200

    Warning messages work for register page

--- Routes/users.js
+++ Routes/users.js
@@ -1,10 +1,47 @@
 const express_ = require('express')
 const router = express_.Router()
+const user = require('../models/user.js')
 
-// login page for user
+// login page for user - when user requests login page
 router.get('/login', (req, res) => res.render('loginpage'))
 
-// register link for user
+// when user requests for /register then send the register page
 router.get('/Register', (req, res) => res.render('registerpage'))
+
+// handle the information recieved from the login page
+router.post('/Register', (req, res) => {
+  const { name, email, password, ConfirmPassword } = req.body
+  // create an error to handle errors
+  const errors_ = []
+
+  // if the password and the confirmation password matches.
+  if (password !== ConfirmPassword) {
+    errors_.push({ message_: 'Passwords do not match'})
+  }
+
+  // Ensure that all the details have been filled out.
+  if (!name || !email || !password || !ConfirmPassword) {
+    errors_.push({ message_: 'Fill in all details' })
+  }
+
+  // Ensure that the users password is greater than 8 characters.
+  if (password.length < 8) {
+    errors_.push({ message_: 'Password must be greater than 8 characters' })
+  }
+
+  if (errors_.length > 0) {
+    res.render('registerpage', {
+      errors_,
+      name,
+      email,
+      password,
+      ConfirmPassword
+    })
+  } else {
+    res.send('USER has logged in....')
+  }
+})
+
+// handle the information recived from the register page
 
 module.exports = router
--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const express = require('express')
-
 const express_layout = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
-// static files
+// configure the database.
+const database = require('./configurations/mongo').MongoURI
+
+// establish a connection with the database
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('---------------------------Connection established with MongoDB-----------'))
+  .catch(err => console.log(err))
+
+  .then(() => console.log('---------------------------Connection established with MongoDB-----------'))
+  .catch(err => console.log(err))
+
+// to access the css files..
 // app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 
-// the middleware
+// the middleware for EJS
 app.use(express_layout)
 app.set('view engine', 'ejs')
 
-// routes
+// Body parser for the forms
+app.use(express.urlencoded({ extended: false }))
+
+// setting up the routes
 app.use('/', require('./Routes/index'))
 app.use('/users', require('./Routes/users'))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 
 app.listen(PORT, console.log('server running...'))
