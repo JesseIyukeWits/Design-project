5667450358c2d2ad70c4c7ae595d5002f8ba2070
Ishvani Moodley <ishvani.moodley@students.wits.ac.za>
2022-08-25 18:18:48 +0200

    I am able to extract and present data on the web from mongo

--- Routes/menu.js
+++ Routes/menu.js
@@ -5,7 +5,11 @@ const user_ = require('../models/user')
 
 router.get('/dashboard', (req, res) => res.render('dashboard'))
 router.get('/log', (req, res) => res.render('logDevice'))
+router.get('/view', (req, res) => res.render('EnergyConsumption'))
+router.get('/display', (req, res) => res.render('display'))
 
+
+// function to handle logging a device.
 router.post('/log', (req, res) => {
   const { username, sensor, Vehicle, Year, ChannelId } = req.body
   console.log(username, sensor, Vehicle, Year, ChannelId)
@@ -29,7 +33,26 @@ router.post('/log', (req, res) => {
       }
     })
     .catch(err => console.log(err))
-
 })
 
+/* The following function extracts information from the database and displays it,
+on the display.ejs page.
+*/
+
+router.post('/view', (req, res) => {
+  const { channelID } = req.body
+  console.log(channelID)
+
+  VehicleSchema.findOne({ ChannelId: channelID })
+    .then(device => {
+      if (device) {
+        res.render('display', { VehicleModel: device.VehicleModel })
+      } else {
+        res.send('DOES NOT EXIST')
+      }
+    })
+    .catch(err => console.log(err))
+  // res.render('display', { show: channelID })
+
+})
 module.exports = router
