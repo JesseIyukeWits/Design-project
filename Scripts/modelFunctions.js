caee6858837ca0c51740433e8da9df990ceefb3b
JesseIyukeWits <88620378+JesseIyukeWits@users.noreply.github.com>
2022-08-28 11:56:21 +0200

    Checked if keys are valid

---
+++ Scripts/modelFunctions.js
@@ -0,0 +1,153 @@
+// Connect flespi to here
+
+
+//Model functions
+async function timeStamp(){
+    try{
+        let response = await require("./response.json") // returns as json object(key-value pair)
+        let timeArr=[]
+        timeArr= response.result.map((user) => user.timestamp) // convert to an array
+        //console.log(timeArr) 
+
+         //remove index contaning undefined values
+        let arr= checkKeys()
+        //console.log(arr.length) 
+        for(let y=0;y<arr.length;y++)
+        {
+            timeArr.splice(arr[y],1) 
+        } 
+        //console.log(timeArr) 
+
+        return timeArr
+   
+    }catch(e){
+        return -1
+    }
+  }
+
+
+  async function altitude(){
+    try{
+        let response = await require("./response.json") // returns as json object(key-value pair)
+        let altitudeArr=[]
+        altitudeArr= response.result.map((user) => user["position.altitude"]) // convert to an array
+
+        //remove index contaning undefined values
+        let arr= checkKeys()
+        for(let y=0;y<arr.length;y++)
+        {
+            altitudeArr.splice(arr[y],1) 
+        } 
+
+        //console.log(altitudeArr) 
+        return altitudeArr
+   
+    }catch(e){
+        return -1
+    }
+  }
+
+  async function latitude(){
+    try{
+        let response = await require("./response.json") // returns as json object(key-value pair)
+        let latitudeArr=[]
+        latitudeArr= response.result.map((user) => user["position.latitude"]) // convert to an array
+
+        let arr= checkKeys()
+        for(let y=0;y<arr.length;y++)
+        {
+            latitudeArr.splice(arr[y],1) 
+        } 
+        //console.log(latitudeArr.length) 
+        return latitudeArr
+   
+    }catch(e){
+        return -1
+    }
+  }
+
+  async function longitude(){
+    try{
+        let response = await require("./response.json") // returns as json object(key-value pair)np
+        let longitudeArr=[]
+        longitudeArr= response.result.map((user) => user["position.longitude"]) // convert to an array
+
+        //remove index contaning undefined values
+        let arr= checkKeys()
+        for(let y=0;y<arr.length;y++)
+        {
+            longitudeArr.splice(arr[y],1) 
+        } 
+       //console.log(longitudeArr)
+       return longitudeArr
+   
+    }catch(e){
+        return -1
+    }
+  }
+
+
+  async function speed(){
+    try{
+        let response = await require("./response.json") // returns as json object(key-value pair)
+        let speedArr=[]
+        speedArr= response.result.map((user) => user["position.speed"]) // convert to an array
+
+        //remove index contaning undefined values
+        let arr= checkKeys()
+        for(let y=0;y<arr.length;y++)
+        {
+            speedArr.splice(arr[y],1) 
+        } 
+        //console.log(speedArr)
+        return speedArr
+   
+    }catch(e){
+        return -1
+    }
+  }
+
+  async function ID(){
+    try{
+        let response = await require("./response.json") // returns as json object(key-value pair)np
+        let IDArr=[]
+        IDArr= response.result.map((user) => user["channel.id"]) // convert to an array
+       // console.log(IDArr)
+        return IDArr
+   
+    }catch(e){
+        return -1
+    }
+  }
+
+
+//Check for undefined keys
+function checkKeys(){
+
+    let arr= []
+        let response = require("./response.json") // returns as json object(key-value pair)
+        //console.log(response.result.length)
+        for(let x=0; x<response.result.length-1; x++){
+            if(response.result[x]["position.latitude"]== undefined || response.result[x]["position.longitude"]== undefined || (response.result[x]["timestamp"]===response.result[x+1]["timestamp"])) {
+                // || (response.result[x]["timestamp"]==response.result[x+1]["timestamp"])
+                arr[x]=x
+                //console.log(arr[x]) // print out index
+            }
+            //console.log(arr)
+        }
+
+        //remove undefined values
+        arr = arr.filter(function( element ) {
+            return element !== undefined;
+         })
+
+        //console.log(arr)
+        return arr
+
+}
+
+
+ module.exports={timeStamp, altitude, latitude, longitude,speed, ID}
+
+ //timeStamp()
+
